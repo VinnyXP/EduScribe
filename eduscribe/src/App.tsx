f2139ae35009f2a7f1f52dc12b7df83133a8e5c0
VinnyXP <viph0009@gmail.com>
2023-10-29 09:47:22 -0700

    remove the non used hook

--- eduscribe/src/App.tsx
+++ eduscribe/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button";
 import { Input } from "./components/ui/input";
-import { useAction, useQuery} from "convex/react";
+import { useAction } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { Textarea } from "./components/ui/textarea";
 //import { useUser } from "@clerk/clerk-react";
@@ -55,63 +55,3 @@ export default function App() {
   );
 }
 
-/*function SignedIn() {
-  const { numbers, viewer } =
-    useQuery(api.myFunctions.listNumbers, {
-      count: 10,
-    }) ?? {};
-  const addNumber = useMutation(api.myFunctions.addNumber);
-
-  return (
-    <>
-      <p>Welcome {viewer}!</p>
-      <p className="flex gap-4 items-center">
-        This is you:
-        <UserButton afterSignOutUrl="#" />
-      </p>
-      <p>
-        Click the button below and open this page in another window - this data
-        is persisted in the Convex cloud database!
-      </p>
-      <p>
-        <Button
-          onClick={() => {
-            void addNumber({ value: Math.floor(Math.random() * 10) });
-          }}
-        >
-          Add a random number
-        </Button>
-      </p>
-      <p>
-        Numbers:{" "}
-        {numbers?.length === 0
-          ? "Click the button!"
-          : numbers?.join(", ") ?? "..."}
-      </p>
-      <p>
-        Edit{" "}
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          convex/myFunctions.ts
-        </code>{" "}
-        to change your backend
-      </p>
-      <p>
-        Edit{" "}
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          src/App.tsx
-        </code>{" "}
-        to change your frontend
-      </p>
-      <p>
-        Check out{" "}
-        <a
-          className="font-medium text-primary underline underline-offset-4"
-          target="_blank"
-          href="https://docs.convex.dev/home"
-        >
-          Convex docs
-        </a>
-      </p>
-    </>
-  );
-}*/
